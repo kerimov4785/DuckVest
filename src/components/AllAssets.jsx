662975c70b0354e946dd29e3463526ccfd2e8013
kerimov4785 <nihatkerimov4785@gmail.com>
2025-06-30 10:23:22 +0400

    watchfixed

--- src/components/AllAssets.jsx
+++ src/components/AllAssets.jsx
@@ -2,16 +2,19 @@ import { Bookmark } from 'lucide-react'
 import React, { useEffect, useState } from 'react'
 import { FaBookmark, FaRegBookmark } from 'react-icons/fa6'
 
-function AllAssets({ stocks, id, letJarr ,allStock, setAllStock}) {
+function AllAssets({ stocks, setStocks, id, letJarr, allStock, setAllStock }) {
     const toggleLike = (stockId) => {
         let obj = allStock.find(item => item.stockID == id)
         if (!stocks.find(item => item.stockID == stockId)) {
             fetch(`http://localhost:4040/watchlist/add-stock=${stockId}-to-wl=${id}`, { method: "POST" })
-            letJarr([...stocks, obj])
+                .then(letJarr([...stocks, obj]))
+
         }
         else {
             fetch(`http://localhost:4040/watchlist/rmv-stock=${stockId}-to-wl=${id}`, { method: "POST" })
-            letJarr(stocks.filter(item => item.stockID != id))
+                .then(
+                    letJarr(stocks.filter(item => item.stockID != id))
+                )
 
         }
     };
@@ -26,7 +29,7 @@ function AllAssets({ stocks, id, letJarr ,allStock, setAllStock}) {
                             <p>{item.symbol}</p>
                         </div>
                         <div style={{ cursor: 'pointer' }} onClick={() => toggleLike(item.stockID)} >
-                            {stocks.find(item2 => item2.stockID == item.stockID ) ? <FaBookmark size={22} color='rgb(255 113 37 / 62%)' /> : <FaRegBookmark size={22} color='white' />
+                            {stocks.find(item2 => item2.stockID == item.stockID) ? <FaBookmark size={22} color='rgb(255 113 37 / 62%)' /> : <FaRegBookmark size={22} color='white' />
                             }
                         </div>
                     </div>
